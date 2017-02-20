18758c4b962c5e6179adcafcfca12f578fee9669
Casey <cecolby11@users.noreply.github.com>
2017-02-19 21:10:30 -0600

    handling gameplay defeat, reorganizing objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,70 @@
 // wrap in document ready bc jquery
 $(document).ready(function() {
 
-// game begins
-  var setup = {
+  //vars and functions realted to general browser display/setup 
+  var browser = {
     "userChar": null,
     "enemies": [],
     "defender": null, 
 
-    showBtn : function(buttonClass) {
-      var attackBtn = $(buttonClass);
-      attackBtn.css("display","block")
+    showBtn: function(buttonClass, shouldShow) {
+      var button = $(buttonClass);
+      if (shouldShow===true) {
+        button.css("display","block");
+        console.log("revealing button!");
+      }
+      else {
+        button.css("display", "none");
+        console.log("hiding button!");
+      }
     },
 
-    displayData : function(buttonSelector) {
+    displayData: function(buttonSelector) {
       var tempDiv = $("<div>" + buttonSelector.attr("healthPoints") + "</div>");
       tempDiv.addClass("char-hp");
       var tempDiv2 = $("<div>" + buttonSelector.attr("name") + "</div>");
       tempDiv2.addClass("char-name");
       buttonSelector.append(tempDiv2, tempDiv);
+    },
+
+    updateHPOnScreen: function() {
+      var userHP = this.userChar.attr("healthPoints");
+      var defHP = this.defender.attr("healthPoints");
+      $(".user-char-btn .char-hp").html(userHP);
+      $(".defender-btn .char-hp").html(defHP);
+    }, 
+
+    updateBattleText: function(textType) {
+      var battleTextDiv = $(".battle-text");
+      //choose the corresponding text content
+      if(textType==="attack"){
+        var sentence = ("You attacked " + character.defName + " with " + character.userAttack + " and " + character.defName + " countered with " + character.defCounter + ".");
+      } else if(textType==="loseGame") {
+        var sentence = "Oh no, you've been defeated! Click 'Play Again' to start a new game.";
+      } else if(textType==="winBattle") {
+        var sentence = ("You defeated " + character.defName + "! Choose your next opponent by clicking an enemy.");
+      } else if(textType==="beginGame") {
+        var sentence = "Preparing to battle opponent. Use the attack button to battle " + character.defName + ".";
+      } else if(textType==="winGame") {
+        var sentence = "Way to go, you are the ultimate champion! Click 'Play Again' to start a new game";
+      }
+      // update the content in the div with the selected text
+      battleTextDiv.html(sentence);
     }
   };
 
+  // vars and functions related to character creation/selection 
   var character = {
-    "name": ["Rachel", "Phoebe", "Chandler", "Joey"],
+    "name": ["Rachel", "Phoebe", "Joey", "Chandler"],
     "healthPoints": [120,100,150,180],
     "attackPower": [8, 5, 20, 25],
     "charBtnArray": [],
+
+    "defName": null,
+    "userHP": null,
+    "defHP": null,
+    "userAttack": null,
+    "defCounter": null,
 
     createCharBtns : function() {
       for(var i = 0; i < this.name.length; i++) {
@@ -38,27 +77,24 @@ $(document).ready(function() {
         // add to array of buttons
         this.charBtnArray.push(charBtn); 
         // add display text 
-        setup.displayData(charBtn);
+        browser.displayData(charBtn);
         //charBtn.text(charBtn.attr("name"));
         // append child to parent element so it displays
         $(".your-char-div").append(charBtn)
       }
-    }
-  };
-
-  var gameplay = {
+    },
 
     // player clicks one of 4 character buttons
     selectCharacter: function() {
       $(".character-button").on("click", function() {
         // only set userChar once per game
-        if(setup.userChar===null){
-          //store selected in setup.userChar 
-          setup.userChar = $(this);
+        if(browser.userChar===null){
+          //store selected in browser.userChar 
+          browser.userChar = $(this);
           // add class user-char
-          setup.userChar.addClass("user-char-btn");
-          //call setup enemies
-          gameplay.addEnemies();
+          browser.userChar.addClass("user-char-btn");
+          //call browser enemies
+          character.addEnemies();
         } else {
           console.log("character already selected!")
         }
@@ -68,9 +104,9 @@ $(document).ready(function() {
     // the others in charBtnArray stored in "enemies"
     addEnemies: function() {
       for(var i = 0; i < character.charBtnArray.length; i++) {
-        if(!(character.charBtnArray[i].attr("name")===setup.userChar.attr("name"))){
-          //add to enemies array in setup
-          setup.enemies.push(character.charBtnArray[i]);
+        if(!(character.charBtnArray[i].attr("name")===browser.userChar.attr("name"))){
+          //add to enemies array in browser
+          browser.enemies.push(character.charBtnArray[i]);
           // add enemy class to charBtns for styling change
           character.charBtnArray[i].addClass("enemy-btn");
           // .enemies is new parent DIV, move to new section of page. 
@@ -84,89 +120,125 @@ $(document).ready(function() {
     selectDefender: function() {
       $(".enemy-btn").on("click", function() {
         //pick defender one time 
-        if(setup.defender===null){
-          //add to setup.defender 
-          setup.defender = $(this);
-          console.log("defender chosen: " + (setup.defender).attr("name"));
+        if(browser.defender===null){
+          //add to browser.defender 
+          browser.defender = $(this);
+          console.log("defender chosen: " + (browser.defender).attr("name"));
           //add defender class for styling change
-          setup.defender.addClass("defender-btn");
+          browser.defender.addClass("defender-btn");
           // .defender is new parent DIV, move to new parent/section of page
-          $(".defender-div").append(setup.defender);
+          $(".defender-div").append(browser.defender);
+          //update all the info in vars so we can access easily
+          character.storeAttributes();
           //show attack button, get ready to fight! 
-          setup.showBtn(".attack-button");
+          browser.showBtn(".attack-button", true);
+          browser.updateBattleText("beginGame");
           battle.attackDefender();
         } else {
           console.log("defender already selected!")
         }
+
       });
+    },
+
+    storeAttributes: function() {
+      //make some vars to keep things easy to access
+      this.defName = browser.defender.attr("name");
+      this.userHP = browser.userChar.attr("healthPoints");
+      this.defHP = browser.defender.attr("healthPoints");
+      this.userAttack = browser.userChar.attr("attackPower");
+      this.defCounter = browser.defender.attr("attackPower");
     }
   };
 
+  // vars and functions related to a user/defender battle
   var battle = {
-    "defName": null,
-    "userHP": null,
-    "defHP": null,
-    "userAttack": null,
-    "defCounter": null,
-
-    updateBattleText: function() {
-      var sentence1 = ("You attacked " + this.defName + " with " + this.userAttack);
-      var sentence2 = (this.defName + " countered with " + this.defCounter);
-      var battleTextDiv = $(".battle-text");
-      battleTextDiv.html(sentence1 + " and " + sentence2);
-    },
 
     updateHealthPoints: function() {
-      this.userHP = this.userHP - this.defCounter;
-      //update new userChar HP in the stored attribute
-      setup.userChar.attr("healthPoints", this.userHP);
-      //TODO update the HP number in the div on screen
+      // user hp decreased by whatever the opponent's counter attack power is 
+      character.userHP = character.userHP - character.defCounter;
+      // each attack click, opponent's hp is decreased by the user's attach power 
+      character.defHP = character.defHP - character.userAttack;
+      //update new HP in the stored attribute on the button
+      browser.userChar.attr("healthPoints", character.userHP);
+      browser.defender.attr("healthPoints", character.defHP);
+      //update the HP number in the div on screen
+      browser.updateHPOnScreen();
+      //see if anyone has been defeated at the new HP levels
+      battle.defeatChecker();
+    },
+
+    updateUserAttackPower: function() {
+      // after every attack click, the user's attack power increases by their base attack power 
+      character.userAttack = parseInt(character.userAttack) + parseInt(character.userAttack);
+      //auto updated in attack text next attack
     },
 
     attackDefender: function() {
-      //update with the values 
-      this.defName = setup.defender.attr("name");
-      this.userHP = setup.userChar.attr("healthPoints");
-      this.defHP = setup.defender.attr("healthPoints");
-      this.userAttack = setup.userChar.attr("attackPower");
-      this.defCounter = setup.defender.attr("attackPower");
-
       $(".attack-button").on("click", function() {
-        battle.updateBattleText();
+        browser.updateBattleText("attack");
         battle.updateHealthPoints();
+        battle.updateUserAttackPower();
       });
+    },
 
+    //check if user or defender defeated
+    defeatChecker: function() {
+      // user defeat
+      if(browser.userChar.attr("healthPoints") <= 0){
+        console.log("you lose!");
+        // show defeat text
+        browser.updateBattleText("loseGame");
+        //hide attack button 
+        browser.showBtn(".attack-button", false);
+        // show 'try again' button which calls some function
+        browser.showBtn(".new-game-button", true);
+      } 
+      // defender defeat 
+      else if(browser.defender.attr("healthPoints") <= 0){
+        console.log("defeated defender!")
+        // remove defeated defender button element! 
+        $(".defender-btn").remove();
+        //hide attack button so it can't be clicked (character.selectdefender will show attack button again)
+        browser.showBtn(".attack-button", false);
+        // show success text (choose a new defender)
+        browser.updateBattleText("winBattle");
+        // set browser.defender = null so character.selectDefender() triggers selection of a new one (using .enemy onclick)
+        browser.defender = null;
+      }
     }
 
   };
 
-  //setup 
+  //browser 
   character.createCharBtns();
   // user interaction 
-  gameplay.selectCharacter();
+  character.selectCharacter();
 
- 
 
-// each time they click, their hp are decreased by whatever the opponent's counter attack power is 
+//remove defender from enemies array when defeated
+// when enemies array is empty, then game over Win! 
 
-// each attack click, opponent's hp is decreased by the user's attach power 
-
-// after every attack click, the user's attack power increases by their base attack power 
-
-//opponent 'defeated' and disappars if their hp gets to 0 or less --> set setup.defender to null so when enemies clicked it will pick a new defender! 
-
-//player can click on a new character and they go from enemies to defender. now attacks are against them. 
-
+//Try again button onclick --> newgame function 
+    // resets everything 
 // game over if users hp gets to 0 or less
-
 // game over if all opponents are defeated
 
-// when game ends, user can hit button to play again and start over
 
 // any character must be able to win or lose if you pick opponents in correct order 
+
+// attack power for userChar shouldn't reset between opponents, it should stay increased from the previous. 
 
 });
 
 // TODO: 
 
 // make divs to display name, hp, etc.displaying just name for now. 
+
+
+// Final housekeeping TODOs: 
+
+//check for semicolons
+// remove any blank lines at end of file
+// remove console.logs 
+// rewrite comments to be very clear and concise. try Brian's suggestoin of a function block comment for each 
